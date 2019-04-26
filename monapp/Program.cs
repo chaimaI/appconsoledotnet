52425a944058ea091e8a835be7e744c9ff6223b9
Imamou <c.imamou@outlook.com>
2019-04-26 12:28:49 +0200

    nouvelle modif

--- monapp/Program.cs
+++ monapp/Program.cs
@@ -6,7 +6,7 @@ namespace monapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bonjour World!");
         }
     }
 }
